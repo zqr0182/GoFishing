378912f0368fc8e6e72c48bf7f4bac5fba6a5a49
zqr0182 <zqr0182@hotmail.com>
2015-08-18 13:21:10 -0400

    Add unit tests

--- GoFishing.Presentation.Tests/Controllers/GoFishingControllerTest.cs
+++ GoFishing.Presentation.Tests/Controllers/GoFishingControllerTest.cs
@@ -14,24 +14,32 @@ using GoFishing.Domain.Models;
 
 namespace GoFishing.Presentation.Tests.Controllers
 {
-    
+    public interface IMockLogger : ILogger
+    {
+        string Message {get; set;}
+
+    }
+ 
     [TestClass]
     public class GoFishingControllerTest
     {
         
         private readonly GoFishingAPIController _goFishingApiController;
         private readonly Mock<IFishingService> _fishingServiceMock;
-        private readonly Mock<ILogger> _loggerMock;
+        private readonly Mock<IMockLogger> _loggerMock;
+        const string ExpectedMessage = "message to log";
+        const string ExceptionMessage = "exception message to log";
+
 
         public GoFishingControllerTest()
         {
             _fishingServiceMock = new Mock<IFishingService>();
-            _loggerMock = new Mock<ILogger>();
+            _loggerMock = new Mock<IMockLogger>();
             _goFishingApiController = new GoFishingAPIController(_fishingServiceMock.Object, _loggerMock.Object);
         }
         
         [TestMethod]
-        public void Can_Get_All_Trips()
+        public void GetListAllTrips_GetTrips()
         {
             _fishingServiceMock.Setup(f => f.GetTrips()).Returns(SetupTrips());
 
@@ -43,15 +51,52 @@ namespace GoFishing.Presentation.Tests.Controllers
         }
 
         [TestMethod]
-        public void Can_Write_Log_When_Get_All_Trips()
+        public void GetListAllTrips_Write_To_Log()
         {
-            _loggerMock.Setup(l => l.Info("Logged"));
-          
-            _fishingServiceMock.Setup(f => f.GetTrips()).Returns(SetupTrips());
 
-            _goFishingApiController.GetListAllTrips();
+            _loggerMock.SetupProperty(l => l.Message, ExpectedMessage);
+
+            _fishingServiceMock.Object.GetTrips();
+
+            Assert.AreEqual(ExpectedMessage, _loggerMock.Object.Message);
+        }
+
+        [TestMethod]
+        public void GetListAllTrips_GetTrips_ThrowException()
+        {
+            _fishingServiceMock.Setup(f => f.GetTrips()).Throws(new Exception(ExceptionMessage));
+
+            AssertException.Throws<Exception>(() => _fishingServiceMock.Object.GetTrips());
+        }
 
 
+        [TestMethod]
+        public void GetListAllTrips_GetTrips_ThrowException_Write_Log()
+        {
+            _loggerMock.SetupProperty(l => l.Message, ExceptionMessage);
+            
+            _fishingServiceMock.Setup(f => f.GetTrips()).Throws(new Exception(ExceptionMessage));
+
+            Assert.AreEqual(ExceptionMessage, _loggerMock.Object.Message);
+        }
+
+        public static class AssertException
+        {
+            public static T Throws<T>(Action action) where T : Exception
+            {
+                try
+                {
+                    action();
+                }
+                catch (T ex)
+                {
+                    return ex;
+                }
+
+                Assert.Fail("Expected exception of type {0}.", typeof(T));
+
+                return null;
+            }
         }
 
         public List<Trip> SetupTrips()
